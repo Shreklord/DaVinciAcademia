b72446489dea4796b53a35f0ed57e669fd37aaa5
Shreklord <goldhama@email.sc.edu>
2024-02-22 16:10:08 -0500

    #22 UserList Stubs updated

--- UserList.java
+++ UserList.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class UserList {
+    private static UserList userList;
+    private ArrayList<User> users;
     
+    private UserList(){
+        
+    }
+
+    public UserList getInstance(){
+        return userList;
+    }
+
+    public User getUser(String userName){
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getUsername().equals(userName)){
+                return users.get(i);
+            }
+        }
+        return null;
+    }
+
+    public User getUser(UUID id){
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getID().equals(id)){
+                return users.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void setInstance(UserList userList){
+        this.userList = userList;
+    }
 }

4c0108d77100acf14c933da1802ef9c90ec5cd36
Shreklord <goldhama@email.sc.edu>
2024-02-22 14:24:48 -0500

    added course stub

--- Course.java
+++ Course.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Course {
-    
+    private UUID id;
+    private int hours;
+    private String subject;
+    private int coursenumber;
+    private ArrayList<String> prereqs;
+
+    public Course(int hours, String subject, int coursenumber, ArrayList<String> prereqs){
+
+    }
+
+    public ArrayList<Course> getPrereqs(){
+        return null;
+    }
+
 }

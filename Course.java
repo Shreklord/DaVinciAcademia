34890ef7f4f238dc85019a165f3d32d43cafdc9d
Shreklord <goldhama@email.sc.edu>
2024-02-22 15:42:22 -0500

    Course update

--- Course.java
+++ Course.java
@@ -8,12 +8,51 @@ public class Course {
     private int coursenumber;
     private ArrayList<String> prereqs;
 
-    public Course(int hours, String subject, int coursenumber, ArrayList<String> prereqs){
-
+    public Course(UUID id, int hours, String subject, int coursenumber, ArrayList<String> prereqs){
+        this.id = id;
+        this.hours = hours;
+        this.subject = subject;
+        this.coursenumber = coursenumber;
+        this.prereqs = prereqs;
     }
 
-    public ArrayList<Course> getPrereqs(){
-        return null;
+    public ArrayList<String> getPrereqs(){
+        return this.prereqs;
     }
+
+    public void setPrereqs(ArrayList<String> prereqs){
+        this.prereqs = prereqs;
+    }
+
+    public int getCourseNumber(){
+        return this.coursenumber;
+    }
+
+    public void setCourseNumber(int coursenumber){
+        this.coursenumber = coursenumber;
+    }
+
+    public String getSubject(){
+        return this.subject;
+    }
+
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+
+    public int getHours(){
+        return this.hours;
+    }
+
+    public void setHours(int hours){
+        this.hours = hours;
+    }
+
+    public UUID getID(){
+        return this.id;
+    }
+
+
+
 
 }

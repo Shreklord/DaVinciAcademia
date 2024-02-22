b706c37c447d3ed0d91afefdef98122bf23a4f4f
Oliver Meihls <koda@Olivers-MacBook-Air.local>
2024-02-22 14:02:50 -0500

    Added method stubs for DataLoader, DataWriter

---
+++ DataLoader.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataLoader {
+
+    public ArrayList<User> getUsers() {
+        return null;
+    }
+
+    public HashMap<Course, String> getCourses() {
+        return null;
+    }
+    public ArrayList<Major> getMajors() {
+        return null;
+    }
+}

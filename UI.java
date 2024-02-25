41750da2ac6590023dc813e2fe61034165262673
sphilips04 <sphilips@email.sc.edu>
2024-02-25 16:36:10 -0500

    #28 create stubs for UI class methods

--- UI.java
+++ UI.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class UI {
-    
+    private Scanner scanner;
+    private Facade facade;
+
+    public void main(String[] args) {
+        displayLoginMenu();
+    }
+
+    public void displayLoginMenu() {
+        System.out.println("Hello DaVinci Academia Students!");
+    }
 }

2c2e6c25e26c202d3e9af7a3e24a4de225df16fb
Relavate <80731060+Relavate@users.noreply.github.com>
2024-02-25 20:41:41 -0500

    #24 Created MajorList class method stubs

--- Faculty.java
+++ Faculty.java
@@ -3,11 +3,13 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class Faculty extends User{
-    private ArrayList<Student> assignedStudents = new ArrayList<>();
-    private ArrayList<Student> getAssignedStudents = new ArrayList<>();
+    private ArrayList<Student> assignedStudents = new ArrayList<Student>();
+    private ArrayList<Student> getAssignedStudents = new ArrayList<Student>();
 
-    public Faculty(){
-        
+    public Faculty(UUID id, String username, String password, String firstName, String lastName) {
+        super(id, username, password, firstName, lastName);
+        this.setAssignedStudents(assignedStudents);
+        this.setGetAssignedStudents(getAssignedStudents);
     }
 
     public void addCourse(HashMap<Course, Integer> courses) {
@@ -29,8 +31,21 @@ public class Faculty extends User{
 
     }
     public ArrayList<Student> getAssignedStudents() {
-        return null;
+        return assignedStudents;
     }
+
+    public void setAssignedStudents(ArrayList<Student> assignedStudents) {
+        this.assignedStudents = assignedStudents;
+    }
+
+    public ArrayList<Student> getGetAssignedStudents() {
+        return getAssignedStudents;
+    }
+
+    public void setGetAssignedStudents(ArrayList<Student> getAssignedStudents) {
+        this.getAssignedStudents = getAssignedStudents;
+    }
+
     public void addNote(Student student) {
         
     }
--- MajorList.java
+++ MajorList.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class MajorList {
-    
+    private MajorList majorList;
+    private ArrayList<Major> majors = new ArrayList<Major>();
+
+    public MajorList() {
+        this.setMajors(majors);
+    }
+
+    public MajorList getMajorList() {
+        return majorList;
+    }
+
+    public ArrayList<Major> getMajors() {
+        return majors;
+    }   
+
+    public void setMajorList(MajorList majorList) {
+        this.majorList = majorList;
+    }
+
+    public void setMajors(ArrayList<Major> majors) {
+        this.majors = majors;
+    }
+
+    public MajorList getInstance() {
+        return null;
+    }
+
+    public Major getMajor(String major) {
+        return null;
+    }
+
+    public Major getByUUID(UUID id) {
+        return null;
+    }
 }

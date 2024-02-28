c244ea7d799ea01c690e840d06aedcc0a79e4bb2
Oliver Meihls <omeihls@email.sc.edu>
2024-02-28 18:29:47 -0500

    Finished saveMajors and saveCourses Methods

--- DataWriter.java
+++ DataWriter.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -8,10 +9,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class DataWriter extends DataConstants {
+    // ArrayList<Student> students = new ArrayList<Student>();
+    // ArrayList<Faculty> faculty = new ArrayList<Faculty>();
+    // ArrayList<Course> courses = new ArrayList<Course>();
     
-    public boolean saveStudents() {
+    public static boolean saveStudents() {
         try {
-            FileWriter fileWriter = new FileWriter(STUDENT_FILE_PATH);
+            FileWriter writer = new FileWriter(STUDENT_FILE_PATH);
             // JSONParser parser = new JSONParser();
             // JSONArray studentsJSON = (JSONArray)new JSONParser().parse(reader);
 
@@ -34,36 +38,123 @@ public class DataWriter extends DataConstants {
         }
         catch (Exception e) {
             e.printStackTrace();
-            return false;
         }
+        return false;
     }
-    public boolean saveStudent(Student student) {
+    public static boolean saveStudent(Student student) {
         return true;
     }
 
-    public boolean saveFaculty() {
+    //NOT WORKING BECAUSE getFacultyJSON() isn't.
+    //NOT TESTED
+    public static boolean saveFaculty() {
+        UserList listOfFaculty = UserList.getInstance();
+        ArrayList<Faculty> allFaculty = listOfFaculty.getFaculty();
+        JSONArray jsonFaculty = new JSONArray();
+
+        for (int i = 0; i < allFaculty.size(); i++) {
+            jsonFaculty.add(getFacultyJSON(allFaculty.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(COURSE_FILE_PATH)) {
+            file.write(jsonFaculty.toJSONString());
+            file.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return true;
     }
-    public boolean saveFacultySingular() {
+
+    //NOT WORKING 
+    //NOT TESTED
+    public static JSONObject getFacultyJSON(Faculty faculty) {
+        JSONObject facultyDetails = new JSONObject();
+        facultyDetails.put("id", faculty.getID());
+        facultyDetails.put("firstName", faculty.getfirstName());
+        facultyDetails.put("lastName", faculty.getLastName());
+        facultyDetails.put("userName", faculty.getUsername());
+        facultyDetails.put("password", faculty.getPassword());
+
+        //NOT WORKING BECAUSE OF THIS BUT I CAN FIX IT PROBABLY MAYBE
+        // getAssignedStudents() returns an arraylist. We will have to loop through each and
+        // get the id from each then append it to the array inside of the json, 
+        // but it is a nested array so it could be funky. Also might have to create
+        // a new array each time and then update it because Arrays cannot change length
+        // dynamically in java booooooooo.
+        facultyDetails.put("assignedStudents", faculty.getAssignedStudents());
+
+        return facultyDetails;
+    }
+    //SHOULD WORK
+    //NOT TESTED
+    public static boolean saveCourses() {
+        CourseList listOfCourses = CourseList.getInstance();
+        ArrayList<Course> allCourses = listOfCourses.getCourses();
+        JSONArray jsonCourses = new JSONArray();
+
+        for (int i = 0; i < allCourses.size(); i++) {
+            jsonCourses.add(getCourseJSON(allCourses.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(COURSE_FILE_PATH)) {
+            file.write(jsonCourses.toJSONString());
+            file.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return true;
     }
 
-    public boolean saveCourses() {
+    public static JSONObject getCourseJSON(Course course) {
+        JSONObject courseDetails = new JSONObject();
+        courseDetails.put("id", course.getID());
+        courseDetails.put("title", course.getTitle());
+        courseDetails.put("courseNumber", course.getCourseNumber());
+        courseDetails.put("hours", course.getHours());
+        courseDetails.put("subject", course.getSubject());
+        courseDetails.put("prereqs", course.getPrereqs());
+
+        return courseDetails;
+    }
+
+    //SHOULD WORK
+    //NOT TESTED
+    public static boolean saveMajors() {
+        MajorList listOfMajors = MajorList.getInstance();
+        ArrayList<Major> allMajors = listOfMajors.getMajors();
+        JSONArray jsonMajors = new JSONArray();
+
+        for (int i = 0; i < allMajors.size(); i++) {
+            jsonMajors.add(getMajorJSON(allMajors.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(MAJOR_FILE_PATH)) {
+            file.write(jsonMajors.toJSONString());
+            file.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return true;
     }
 
-    public boolean saveCourse(Course course) {
+    public static JSONObject getMajorJSON(Major major) {
+        JSONObject majorDetails = new JSONObject();
+        majorDetails.put("id", major.getID());
+        majorDetails.put("name", major.getName());
+        majorDetails.put("type", major.getType());
+        majorDetails.put("hours", major.getHours());
+        majorDetails.put("majorreq", major.getMajorRequirements());
+        majorDetails.put("electivereq", major.getElectiveCourseReqs());
 
-        JSONObject jsonCourse = new JSONObject();
-
-        return true;
-    }
-
-    public boolean saveMajors() {
-        return true;
-    }
-
-    public boolean saveMajor() {
-        return true;
+        return majorDetails;
     }
 }

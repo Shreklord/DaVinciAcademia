9a797822e36a85d13b2dc14539f2a448bc4d8ab2
Oliver Meihls <koda@Olivers-MacBook-Air.local>
2024-03-12 14:17:29 -0400

    DataWriter Student Tested

--- DataWriter.java
+++ DataWriter.java
@@ -12,13 +12,13 @@ public class DataWriter extends DataConstants {
     
     //SHOULD WORK
     //NOT TESTED
-    public static boolean saveStudents() {
-        UserList listOfStudents = UserList.getInstance();
-        ArrayList<Student> allStudents = listOfStudents.getStudents();
+    public static boolean saveStudents(ArrayList<Student> students) {
+    //     UserList listOfStudents = UserList.getInstance();
+    //     ArrayList<Student> allStudents = listOfStudents.getStudents();
         JSONArray jsonStudents = new JSONArray();
 
-        for (int i = 0; i < allStudents.size(); i++) {
-            jsonStudents.add(getStudentJSON(allStudents.get(i)));
+        for (int i = 0; i < students.size(); i++) {
+            jsonStudents.add(getStudentJSON(students.get(i)));
         }
 
         try {
@@ -38,29 +38,29 @@ public class DataWriter extends DataConstants {
 
     public static JSONObject getStudentJSON(Student student) {
         JSONObject studentDetails = new JSONObject();
-        studentDetails.put("id", student.getID());
+        studentDetails.put("id", String.valueOf(student.getID()));
         studentDetails.put("standing", student.getStanding());
         studentDetails.put("firstName", student.getFirstName());
         studentDetails.put("lastName", student.getLastName());
         studentDetails.put("userName", student.getUsername());
         studentDetails.put("password", student.getPassword());
-        studentDetails.put("major", student.getMajor());
+        studentDetails.put("major", student.getMajor().getName());
         studentDetails.put("GPA", String.valueOf(student.getGPA()));
 
         //Define new JSONArray for the courses key
         // for each Student
         JSONArray coursesToAdd = new JSONArray();
 
-        //Loop through all of the student's courses.
+        //Loop through all of the student's courses
         for (int i = 0; i < student.getCourses().size(); i++) {
             //Grabs the current course
             StudentCourse c = (student.getCourses().get(i));
             
             //If the course is taken,
-            // create and fill out the JSONObject with the course's info.
+            // create and fill out the JSONObject with the course's info
             if (c.getIsCompleted()) {
                 JSONObject courseTaken = new JSONObject();
-                courseTaken.put("courseid", c.getID());
+                courseTaken.put("courseid", String.valueOf(c.getID()));
                 courseTaken.put("grade", String.valueOf(c.getGrade()));
                 courseTaken.put("attempts", String.valueOf(c.getAttempts()));
                 courseTaken.put("isCompleted", String.valueOf(c.getIsCompleted()));
@@ -69,7 +69,7 @@ public class DataWriter extends DataConstants {
                 coursesToAdd.add(courseTaken);
             }
         }
-        //Finally, add the JSONArray as the value for "coursestaken"
+        //Finally, add the JSONArray as the value for "coursesTaken"
         studentDetails.put("coursesTaken", coursesToAdd);
         studentDetails.put("notes", student.getNotes());
 
@@ -78,16 +78,16 @@ public class DataWriter extends DataConstants {
 
     //SHOULD WORK
     //NOT TESTED
-    public static boolean saveFaculty() {
-        UserList listOfFaculty = UserList.getInstance();
-        ArrayList<Faculty> allFaculty = listOfFaculty.getFaculty();
+    public static boolean saveFaculty(ArrayList<Faculty> faculty) {
+        // UserList listOfFaculty = UserList.getInstance();
+        // ArrayList<Faculty> allFaculty = listOfFaculty.getFaculty();
         JSONArray jsonFaculty = new JSONArray();
 
-        for (int i = 0; i < allFaculty.size(); i++) {
-            jsonFaculty.add(getFacultyJSON(allFaculty.get(i)));
+        for (int i = 0; i < faculty.size(); i++) {
+            jsonFaculty.add(getFacultyJSON(faculty.get(i)));
         }
 
-        try (FileWriter file = new FileWriter(COURSE_FILE_PATH)) {
+        try (FileWriter file = new FileWriter(FACULTY_FILE_PATH)) {
             file.write(jsonFaculty.toJSONString());
             file.flush();
 
@@ -104,7 +104,7 @@ public class DataWriter extends DataConstants {
     public static JSONObject getFacultyJSON(Faculty faculty) {
         JSONObject facultyDetails = new JSONObject();
 
-        facultyDetails.put("id", faculty.getID());
+        facultyDetails.put("id", String.valueOf(faculty.getID()));
         facultyDetails.put("firstName", faculty.getFirstName());
         facultyDetails.put("lastName", faculty.getLastName());
         facultyDetails.put("userName", faculty.getUsername());
@@ -123,13 +123,13 @@ public class DataWriter extends DataConstants {
     
     //SHOULD WORK
     //NOT TESTED
-    public static boolean saveCourses() {
-        CourseList listOfCourses = CourseList.getInstance();
-        ArrayList<Course> allCourses = listOfCourses.getCourses();
+    public static boolean saveCourses(ArrayList<Course> courses) {
+        // CourseList listOfCourses = CourseList.getInstance();
+        // ArrayList<Course> allCourses = listOfCourses.getCourses();
         JSONArray jsonCourses = new JSONArray();
 
-        for (int i = 0; i < allCourses.size(); i++) {
-            jsonCourses.add(getCourseJSON(allCourses.get(i)));
+        for (int i = 0; i < courses.size(); i++) {
+            jsonCourses.add(getCourseJSON(courses.get(i)));
         }
 
         try (FileWriter file = new FileWriter(COURSE_FILE_PATH)) {
@@ -146,7 +146,7 @@ public class DataWriter extends DataConstants {
 
     public static JSONObject getCourseJSON(Course course) {
         JSONObject courseDetails = new JSONObject();
-        courseDetails.put("id", course.getID());
+        courseDetails.put("id", String.valueOf(course.getID()));
         courseDetails.put("title", course.getTitle());
         courseDetails.put("courseNumber", String.valueOf(course.getCourseNumber()));
         courseDetails.put("hours", String.valueOf(course.getHours()));
@@ -165,13 +165,13 @@ public class DataWriter extends DataConstants {
 
     //SHOULD WORK
     //NOT TESTED
-    public static boolean saveMajors() {
-        MajorList listOfMajors = MajorList.getInstance();
-        ArrayList<Major> allMajors = listOfMajors.getMajors();
+    public static boolean saveMajors(ArrayList<Major> majors) {
+        // MajorList listOfMajors = MajorList.getInstance();
+        // ArrayList<Major> allMajors = listOfMajors.getMajors();
         JSONArray jsonMajors = new JSONArray();
 
-        for (int i = 0; i < allMajors.size(); i++) {
-            jsonMajors.add(getMajorJSON(allMajors.get(i)));
+        for (int i = 0; i < majors.size(); i++) {
+            jsonMajors.add(getMajorJSON(majors.get(i)));
         }
 
         try (FileWriter file = new FileWriter(MAJOR_FILE_PATH)) {
@@ -188,7 +188,7 @@ public class DataWriter extends DataConstants {
 
     public static JSONObject getMajorJSON(Major major) {
         JSONObject majorDetails = new JSONObject();
-        majorDetails.put("id", major.getID());
+        majorDetails.put("id", String.valueOf(major.getID()));
         majorDetails.put("name", major.getName());
         majorDetails.put("type", major.getType());
         majorDetails.put("hours", String.valueOf(major.getHours()));
